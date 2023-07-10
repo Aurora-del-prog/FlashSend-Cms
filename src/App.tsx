777d9e6701490b0ab31f031906b6d2900c9e1435
Cc <2744166358@qq.com>
2023-07-10 22:21:12 +0800

    feat: 🎸 主题色

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-
-import './App.less'
-import { BrowserRouter, RouterProvider } from 'react-router-dom'
-import Router from './router'
-import route from './router'
 import { Suspense } from 'react'
+import { BrowserRouter, RouterProvider } from 'react-router-dom'
+import route from './router'
+// import Router from './router'
+
+import { ConfigProvider } from 'antd'
+import './App.less'
+
 function App() {
 	return (
 		//路由懒加载需要添加suspense或者 使用startTransition函数来包装可能导致暂停的更新
-		<Suspense fallback={<div>Loading...</div>}>
-			<RouterProvider router={route}/>
-		</Suspense>
+		<ConfigProvider
+			theme={{
+				token: {
+					colorPrimary: '#ed6c00'
+				}
+			}}
+		>
+			<Suspense fallback={<div>Loading...</div>}>
+				<RouterProvider router={route}/>
+			</Suspense>
+		</ConfigProvider>
 
 		// <BrowserRouter>
 		// 	<Suspense fallback={<div>Loading...</div>}>
